9eeaa98fda409ebb5351a94567f9879978431256
lejunj <lejunj@umich.edu>
2021-01-16 17:03:20 -0500

    WIP on controller command publishing

--- controller/src/controller_main.cpp
+++ controller/src/controller_main.cpp
@@ -10,20 +10,19 @@ int main(int argc, char** argv) {
 
     controller::Controller vehicle_controller;
 
-    ros::Rate loop_rate(10);
-
     ros::Subscriber sensor_subscriber = vehicle_controller._n.subscribe("/carla/ego_vehicle/vehicle_info",
                                                               1000,
                                                               &controller::Controller::callback_egoVehicleControl,
                                                               &vehicle_controller);
 
-    vehicle_controller.calculate();
+    while (ros::ok()) {
+      vehicle_controller.calculate();
 
-    ros::Publisher control_publisher = vehicle_controller._n.advertise<carla_msgs::CarlaEgoVehicleControl>("/carla/ego_vehicle/vehicle_control_cmd", 1000);
-    control_publisher.publish(vehicle_controller.get_msg());
+      ros::Publisher control_publisher = vehicle_controller._n.advertise<carla_msgs::CarlaEgoVehicleControl>("/carla/ego_vehicle/vehicle_control_cmd", 1000);
+      control_publisher.publish(vehicle_controller.get_msg());
 
-    loop_rate.sleep();
-    ros::spin();
+      ros::spinOnce();
+    }
 
     return 0;
 }
